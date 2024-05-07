ed4791282f376b067f664e95a8ea197308c720c1
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-05-07 15:43:22 +0200

    Toggle background transparency on keymap

--- init.lua
+++ init.lua
@@ -126,6 +126,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Toggle background transparency
+vim.keymap.set('n', '<leader>t', ':lua require("custom.colors").ToggleBgTransparency()<CR>', { desc = 'Toggle background transparency', silent = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/custom/colors.lua
+++ lua/custom/colors.lua
@@ -5,10 +5,20 @@ function M.SetColorScheme(color, bg)
   bg = bg or false
   vim.cmd.colorscheme(color)
 
+  vim.g.current_colorscheme = color
+  vim.g.current_bg_transparency = bg
+
   if not bg then
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   end
+end
+
+function M.ToggleBgTransparency()
+  -- vim.g.colors_name does not return the whole name (E.g. tokyonight instead of tokyonight-night)
+  local color = vim.g.current_colorscheme
+  local bg = not vim.g.current_bg_transparency
+  M.SetColorScheme(color, bg)
 end
 
 return M
