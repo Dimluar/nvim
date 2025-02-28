f6a91f27765475f855f0a6ae00d7d5844e431224
Dimluar <148585902+Dimluar@users.noreply.github.com>
2025-02-28 02:22:58 +0100

    Install nvim-tmux-navigation plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,4 +10,18 @@ return {
       options = { theme = 'dracula' },
     },
   },
+  {
+    'alexghergh/nvim-tmux-navigation',
+    opts = {
+      disable_when_zoomed = true, -- defaults to false
+      keybindings = {
+        left = '<C-h>',
+        down = '<C-j>',
+        up = '<C-k>',
+        right = '<C-l>',
+        last_active = '<C-\\>',
+        next = '<C-Space>',
+      },
+    },
+  },
 }
