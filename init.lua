21585417676b9ef06aa890fec726ba407e6d94e5
Dimluar <148585902+Dimluar@users.noreply.github.com>
2025-03-07 23:26:03 +0100

    Enable transparency as default

--- init.lua
+++ init.lua
@@ -697,7 +697,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      colors.SetColorScheme('catppuccin-mocha', true)
+      colors.SetColorScheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
